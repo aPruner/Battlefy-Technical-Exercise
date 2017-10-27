944e429aa87de3b49f864adcd8aa8e3fe78cdee8
Adam Pruner <adampruner99@gmail.com>
2017-10-27 01:32:47 -0700

    add fake data to prod. server

--- dist/server.js
+++ dist/server.js
@@ -7,3 +7,22 @@ app.use(serveStatic(__dirname));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
+var cors = require('cors');
+const apiApp = express();
+
+apiApp.use(cors());
+apiApp.get('/api/:summonerName', (req, res) => {
+  const data = {
+    "profileIconId": 509,
+    "name": "SH0CK",
+    "summonerLevel": 30,
+    "accountId": 41215217,
+    "id": 26522373,
+    "revisionDate": 1508991321000
+  };
+
+  return res.send(data);
+});
+
+apiApp.listen(3001);
